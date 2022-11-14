5f07e09dd2164a825538d912d90fdba59583c7ec
ABOBA <8chewbacca@gmail.com>
2022-11-14 20:05:42 +0500

    Добавил экспорт в JSON файл

--- lab2.py
+++ lab2.py
@@ -1,28 +1,49 @@
 from ftplib import parse150
 from array import *
+import json
 
 class Weapon:
-    def __init__(self, damage = 0, price = 0, name = "unknown weapon", description = "empty description"):
+    def __init__(self, damage = 0, price = 0, description = "empty description"):
         self.damage = damage
-        self.price = price
-        self.name = name
+        self.price = price        
         self.description = description
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 class Armor:
-    def __init__(self, defence = 0, price = 0, name = "unknown armor", description = "empty description"):
+    def __init__(self, defence = 0, price = 0, description = "empty description"):
         self.defence = defence
-        self.price = price
-        self.name = name
+        self.price = price        
         self.description = description
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+class Damage_over_time:
+    def __init__(self, max_duration = 0, damage = 0, description = "empty_description"):
+        self.max_duration = max_duration
+        self.cur_duration = max_duration
+        self.damage = damage
+        self.description = description
+
+    def Tick(self, target):
+        if (self.cur_duration > 0):
+            self.cur_duration -= 1
+            target.Receive_damage(self.damage)
+        else:
+            del self
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 class Character:
-    def __init__(self, health = 100, name = "unknown character", right_hand = Weapon(), left_hand = Weapon(), armor = Armor()):
-        self.health = health
-        self.name = name
+    def __init__(self, health = 100, right_hand = Weapon(), left_hand = Weapon(), armor = Armor()):
+        self.health = health        
         self.right_hand = right_hand
         self.left_hand = left_hand
         self.armor = armor
-        self.effect_list = array(Damage_over_time)
+        self.effect_list = []
 
     def Die(self, damage = 0):
         print(self.name + " received " + damage + " and died!")
@@ -34,31 +55,15 @@ class Character:
         else:
             self.health -= damage
 
-    def Attack(self, target = Character()):
+    def Attack(self, target):
         target.Receive_damage(self.right_hand.damage + self.right_hand.damage - target.armor.defence)
 
-class Damage_over_time:
-    def __init__(self, max_duration = 0, damage = 0, name = "unknown effect", description = "empty_description"):
-        self.max_duration = max_duration
-        self.cur_duration = max_duration
-        self.damage = damage
-        self.name = name
-        self.description = description
-
-    def Tick(self, target = Character()):
-        if (self.cur_duration > 0):
-            self.cur_duration -= 1
-            target.Receive_damage(self.damage)
-        else:
-            del self
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 class Party:
-    def __init__(self, name = "unknown party"):        
-        self.name = name
-        self.members = array(Character)
-
-    def Rename(self, new_name = "unknown name"):
-        self.name = new_name
+    def __init__(self):        
+        self.members = []    
 
     def Add_member(self, char = Character()):        
         self.members.append(char)
@@ -66,3 +71,17 @@ class Party:
     def Delete_member(self, char = Character()):
         if (self.memebers.find(char) >= 0):            
             self.members.pop(self.members.find(char))
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+text_file = open("classes.json", "w")
+guild = Party()
+
+yura = Character()
+frostmourne = Weapon(100, 1000)
+yura.right_hand = frostmourne
+guild.Add_member(yura)
+
+text_file.write(guild.toJSON())
+text_file.close()
