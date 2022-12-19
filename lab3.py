0c3b57d0fd363e948be8c3600ccbd124c4f47cc7
YuraLapin <88285853+YuraLapin@users.noreply.github.com>
2022-12-19 16:03:29 +0500

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -1,1 +1,19 @@
+from random import randint
+from pyDatalog import pyDatalog
+pyDatalog.create_terms('X, Sum, Avg, RandomIntSum, RandomIntMed, Ans')
+pyDatalog.create_terms('Summary, Range, Length, ListSummary, List, Median')
 
+(Summary[Range] == sum_(X, for_each = X)) <= X.in_(range_(Range + 1))
+(Length[Range] == len_(X, for_each = X)) <= X.in_(range_(Range + 1))
+RandomList = [randint(0, 99) for _ in range(0, 100)]
+RandomList.sort()
+(ListSummary[List] == sum_(X, for_each = X)) <= X.in_(List)
+(Ans == Median[List]) <= (Ans == (List[49] + List[50]) / 2)
+
+print(Sum == Summary[8])
+print()
+print(Avg == (Summary[8] / Length[8]))
+print()
+print(RandomIntSum == ListSummary[RandomList])
+print()
+print(RandomIntMed == Median[RandomList])
